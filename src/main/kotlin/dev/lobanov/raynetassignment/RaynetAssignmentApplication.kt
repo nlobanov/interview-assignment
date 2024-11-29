b24216a9172a48362f44d9138fd543953fef78e4
Nikita Lobanov <lobanovnikita@seznam.cz>
2024-11-29 13:52:19 +0100

    init

---
+++ src/main/kotlin/dev/lobanov/raynetassignment/RaynetAssignmentApplication.kt
@@ -0,0 +1,11 @@
+package dev.lobanov.raynetassignment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RaynetAssignmentApplication
+
+fun main(args: Array<String>) {
+    runApplication<RaynetAssignmentApplication>(*args)
+}
---
+++ src/test/kotlin/dev/lobanov/raynetassignment/RaynetAssignmentApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.lobanov.raynetassignment
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RaynetAssignmentApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
